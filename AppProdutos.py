9657165598ce8693c10cf3f2921a4289678d0369
AdamastorPitakuse <adamstorpitakus@gmail.com>
2023-10-23 17:14:41 -0300

    sas

---
+++ AppProdutos.py
@@ -0,0 +1,61 @@
+
+def main_menu():
+    while True:
+        print("\n--- Menu Principal ---")
+        print("1 - Gerenciar Produtos")
+        print("2 - Gerenciar Clientes")
+        print("3 - Gerenciar Fornecedores")
+        print("4 - Gerenciar Vendas")
+        print("5 - Gerenciar Compras")
+        print("0 - Sair")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            gerenciar_produtos()
+        elif opcao == "2":
+            gerenciar_clientes()
+        elif opcao == "3":
+            gerenciar_fornecedores()
+        elif opcao == "4":ss
+            gerenciar_vendas()
+        elif opcao == "5":
+            gerenciar_compras()
+        elif opcao == "0":
+            logging.info("Saindo do sistema.")
+            break
+        else:
+            logging.warning("Opção inválida!")
+
+
+
+def gerenciar_produtos():
+    while True:
+        print("\n--- Gerenciar Produtos ---")
+        print("1 - Consultar Posição de Estoque")
+        print("2 - Cadastrar NOVO Produto")
+        print("3 - Editar Produto")
+        print("4 - Excluir Produto")
+        print("5 - adicionar produtos ao estoque existente-<importação manual>")
+        print("0 - Voltar")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            handler.consultar_posicao_estoque()
+        elif opcao == "2":  
+            handler.cadastrar_produto()
+        elif opcao == "3":
+            handler.editar_produto()
+        elif opcao == "4":
+            handler.excluir_produto()
+        elif opcao == "5":
+            handler.adicionar_produtos()
+        elif opcao == "0":
+            logging.info("Voltando ao Menu Principal.")
+            break
+        else:
+            logging.warning("Opção inválida!")
+
+
+
+if __name__ == "__main__":
+    main_menu()
--- app_teste.py
+++ app_teste.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog, ttk
 from produto import Produto
+import database as db
+import logging
+import handler as handler
+
+logging.basicConfig(filename="logs.log", level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
+
 
 class MainMenu(tk.Frame):
     def __init__(self, root):
@@ -30,11 +36,13 @@ class MainMenu(tk.Frame):
     def open_product_mgmt(self):
         product_mgmt_window = tk.Toplevel(self.root)
         product_mgmt_window.title("Gerenciamento de Produtos")
-        App_produtos(product_mgmt_window)  # Inicializando a classe App_produtos dentro da nova janela
+        # Inicializando a classe AppProdutos dentro da nova janela
+        app = AppProdutos(product_mgmt_window)
 
 
 
-    class App_produtos(tk.Frame):
+
+    class AppProdutos(tk.Frame):
         def __init__(self, root):
             self.root = root
             self.root.title("Menu Interativo de Produtos")
@@ -47,64 +55,108 @@ class MainMenu(tk.Frame):
             self.preview_label.pack(pady=10, padx=10)
 
             # Botão para adicionar produto
-            self.add_button = ttk.Button(root, text="Adicionar Produto", command=self.add_product)
+            self.add_button = ttk.Button(root, text="Adicionar Produto", command=self.add_produtos)
             self.add_button.pack(pady=20)
 
             # Botão para listar produtos
             self.list_button = ttk.Button(root, text="Listar Produtos", command=self.list_products)
             self.list_button.pack(pady=20)
 
-            # Lista para armazenar produtos
-            self.products = []
+            # Botão para editar produto
+            self.edit_button = ttk.Button(root, text="Editar Produto", command=self.edit_product)
+            self.edit_button.pack(pady=20)
 
-        def ask_combobox(self, title, prompt, values):
-            """Função personalizada para exibir um combobox."""
-            dialog = tk.Toplevel(self.root)
-            dialog.title(title)
-            ttk.Label(dialog, text=prompt).pack(pady=10, padx=10)
-            combo_var = tk.StringVar()
-            combobox = ttk.Combobox(dialog, textvariable=combo_var, values=values)
-            combobox.pack(pady=10, padx=10)
-            combobox.set(values[0])
-            ttk.Button(dialog, text="OK", command=dialog.destroy).pack(pady=10)
-            self.root.wait_window(dialog)
-            return combo_var.get()
+            # Botão para excluir produto
+            self.delete_button = ttk.Button(root, text="Excluir Produto", command=self.delete_product)
+            self.delete_button.pack(pady=20)
 
-        def add_product(self):
-            nome = simpledialog.askstring("Input", "Nome do Produto (Apelido ou Nome Amigável):")
-            self.preview_text.set(f"Nome: {nome}")
-            
-            descricao = simpledialog.askstring("Input", "Descrição do Produto:")
-            self.preview_text.set(self.preview_text.get() + f"\nDescrição: {descricao}")
-            
-            codigo_barras = simpledialog.askstring("Input", "Código de Barras (opcional):") or "000000000"
-            self.preview_text.set(self.preview_text.get() + f"\nCódigo de Barras: {codigo_barras}")
-            
-            categoria = self.ask_combobox("Input", "Categoria:", ["Ativo Fixo", "Uso e Consumo", "Revenda", "Utilizar na Prestação de Serviços"])
-            self.preview_text.set(self.preview_text.get() + f"\nCategoria: {categoria}")
-            
-            preco_compra = float(simpledialog.askstring("Input", "Preço de Compra:"))
-            self.preview_text.set(self.preview_text.get() + f"\nPreço de Compra: R${preco_compra}")
-            
-            preco_venda = float(simpledialog.askstring("Input", "Preço de Venda:"))
-            self.preview_text.set(self.preview_text.get() + f"\nPreço de Venda: R${preco_venda}")
-            
-            quantidade = float(simpledialog.askstring("Input", "Quantidade:"))
-            self.preview_text.set(self.preview_text.get() + f"\nQuantidade: {quantidade}")
-            
-            unidade_de_medida = self.ask_combobox("Input", "Unidade de Medida:", Produto.UNIDADES_DE_MEDIDA)
-            self.preview_text.set(self.preview_text.get() + f"\nUnidade de Medida: {unidade_de_medida}")
+            # Botão para voltar ao menu principal
+            self.back_button = ttk.Button(root, text="Voltar ao Menu Principal", command=self.back_to_main_menu)
+            self.back_button.pack(pady=20)
 
-            product = Produto(nome, descricao, codigo_barras, categoria, preco_compra, preco_venda, quantidade, unidade_de_medida)
-            self.products.append(product)
+        def add_produtos(self):
+            try:
+                # Criando uma instância da classe Produto
+                product = Produto()
 
-            messagebox.showinfo("Info", f"Produto '{nome}' adicionado com sucesso!")
+                # Obtendo os dados do produto
+                product.name = simpledialog.askstring("Nome", "Digite o nome do produto:")
+                if not product.name:
+                    raise ValueError("Nome do produto é obrigatório!")
+
+                product.price = simpledialog.askfloat("Preço", "Digite o preço do produto:")
+                if not product.price:
+                    raise ValueError("Preço do produto é obrigatório!")
+
+                product.quantity = simpledialog.askinteger("Quantidade", "Digite a quantidade do produto:")
+                if not product.quantity:
+                    raise ValueError("Quantidade do produto é obrigatória!")
+
+                # Adicionando o produto ao banco de dados
+                handler.add_product(product)
+
+                # Atualizando a pré-visualização
+                self.update_preview(product)
+
+                messagebox.showinfo("Sucesso", "Produto adicionado com sucesso!")
+            except Exception as e:
+                messagebox.showerror("Erro", str(e))
 
         def list_products(self):
-            product_list = "\n".join([str(product) for product in self.products])
-            messagebox.showinfo("Lista de Produtos", product_list or "Nenhum produto adicionado.")
+            # Obtendo todos os produtos do banco de dados
+            products = handler.get_products()
 
-    if __name__ == "__main__":
-        root = tk.Tk()
-        app = App_produtos(root)
-        root.mainloop()
+            # Atualizando a pré-visualização
+            self.update_preview(products)
+
+        def edit_product(self):
+            # Selecionando o produto a ser editado pode pesquisar por id, codigo ou nome
+            product_id = simpledialog.askinteger("ID", "Digite o ID do produto:")
+            product = handler.get_product(product_id)
+            
+            # Verificando se o produto existe
+            if product:
+                # Obtendo os novos dados do produto
+                product.name = simpledialog.askstring("Nome", "Digite o nome do produto:", initialvalue=product.name)
+                product.price = simpledialog.askfloat("Preço", "Digite o preço do produto:", initialvalue=product.price)
+                product.quantity = simpledialog.askinteger("Quantidade", "Digite a quantidade do produto:", initialvalue=product.quantity)
+
+                # Atualizando o produto no banco de dados
+                handler.update_product(product)
+
+                # Atualizando a pré-visualização
+                self.update_preview(product)
+            else:
+                messagebox.showwarning("Produto não encontrado", "O produto com o ID {} não foi encontrado.".format(product_id))
+
+        def delete_product(self):
+            # Selecionando o produto a ser excluído
+            product_id = simpledialog.askinteger("ID", "Digite o ID do produto:")
+            product = handler.get_product(product_id)
+
+            # Verificando se o produto existe
+            if product:
+                # Excluindo o produto do banco de dados
+                handler.delete_product(product_id)
+
+                # Atualizando a pré-visualização
+                self.update_preview(product)
+            else:
+                messagebox.showwarning("Produto não encontrado", "O produto com o ID {} não foi encontrado.".format(product_id))
+        
+        def update_preview(self, product):
+            """Atualiza a pré-visualização com os dados do produto"""
+            if isinstance(product, Produto):
+                self.preview_text.set("ID: {}\nNome: {}\nPreço: {}\nQuantidade: {}".format(product.id, product.name, product.price, product.quantity))
+            elif isinstance(product, list):
+                self.preview_text.set("Produtos: {}".format(len(product)))
+            else:
+                self.preview_text.set("")
+
+        def back_to_main_menu(self):
+            self.root.destroy()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainMenu(root)
+    root.mainloop()
